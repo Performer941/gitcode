249d2b3cf0fd00d2cfef6f82d76c4ab1ae5d857e
performer <15815094902@163.com>
2020-09-23 16:52:29 +0800

    '迁移数据库并导入数据'

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from flask import Flask
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from utils.common import show_top_6_news_style
 from views import index_blu
+from models import db
 
-# 创建flask应用对象
 app = Flask(__name__)
-
+app.config.from_pyfile("config.ini")
 # 创建蓝图，且注册到app
 app.register_blueprint(index_blu)
 
+db.init_app(app)
+# 添加数据库迁移等工具
+manager = Manager(app)
+# 添加过滤器
+app.add_template_filter(show_top_6_news_style)
+# 生成migrate对象 用来数据库迁移
+migrate = Migrate(app, db)
+# 添加db命令
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(port=8899)
+    # app.run(port=8899)
+    manager.run()
